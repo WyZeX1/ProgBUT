7ba9845bf6c4c62e8d82796db0a5648841d8df6f
Aurélien <Aurlien@192.168.1.130>
2024-09-13 18:19:37 +0200

    New changes for Mediatheque class

--- TP_Mediatheque/src/Mediatheque.java
+++ TP_Mediatheque/src/Mediatheque.java
@@ -3,25 +3,43 @@ import java.util.LinkedList;
 
 public class Mediatheque {
 
-	private LinkedList<Document> bibliotheque;
+	private LinkedList<Document> inventaire;
 	
 	public Mediatheque() {
-		this.bibliotheque = new LinkedList<Document>();
+		this.inventaire = new LinkedList<Document>();
 	}
 	
 	public void ajouteDocument(Document d) {
-		bibliotheque.add(d);
+		inventaire.add(d);
 	}
 	
-	public String afficheCatalogue() {
-		return "La médiathèque possède "+bibliotheque.size()+" documents.";
+	public void afficheCatalogue() {
+		System.out.println("La médiathèque possède "+inventaire.size()+" documents.");
+		for (int i=0 ; i < inventaire.size();i++) {
+			System.out.println(inventaire.get(i));
+		}
 	}
 
 	public Document get(int numInventaire) {
-		return "JSP";
+		for (int i=0 ; i < inventaire.size();i++) {
+			if (inventaire.get(i).getNumInventaire() == numInventaire) {
+				return inventaire.get(i);
+			}
+		}
+		return null;
 	}
 
 	public boolean supprimeDocument(int numInventaire) {
+		for (int i=0 ; i < inventaire.size();i++) {
+			if (inventaire.get(i).getNumInventaire() == numInventaire) {
+				inventaire.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
 	
+	public void emprunte(int numInventaire) {
+		
 	}
 }
